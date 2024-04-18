94df65cd6437cda6f4b7fecb8ba0a0d85a6eac5b
Tusharv1107 <165862437+Tusharv1107@users.noreply.github.com>
2024-04-18 10:45:40 +0530

    Create Interactive Menu System for Customer Assistance.py

---
+++ Pythoncode /Interactive Menu System for Customer Assistance.py
@@ -0,0 +1,22 @@
+person = input("What is your name?")
+print("Hello " + person)
+Menu = print("1 for technical Support")
+Account = print("2 for finance Advice")
+Hours = print("3 for Display Office Hour")
+
+choice = float(input("Enter your Choice"))
+print(choice)
+
+choice2 = float(input("Enter your Choice"))
+print(choice2)
+
+print(choice + choice2)
+print(1 + 2)
+
+print(
+    """
+      (|\_/|)
+      (=*.*=)
+      u-----u
+      """
+)
