d5e5929c79ae99b3847fc04702315633f4eabd6e
Tusharv1107 <165862437+Tusharv1107@users.noreply.github.com>
2024-04-18 10:46:22 +0530

    Create Ifelse.py

---
+++ Pythoncode /Ifelse.py
@@ -0,0 +1,8 @@
+# Prompt the user to enter their age
+age = int(input("Please enter your age: "))
+
+# Check if the user is eligible to vote
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
